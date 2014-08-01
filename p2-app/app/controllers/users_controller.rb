8977ceb6ac0a5a2688fe6610f922aa235078cef0
hoanewton <caohoa86hp@gmail.com>
2014-08-01 14:01:44 -0400

    create new user works

--- p2-app/app/controllers/users_controller.rb
+++ p2-app/app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:username, :first_name, :password, :password_confirmation)
+    params.require(:user).permit(:username, :email, :birthday, :first_name, :password, :password_confirmation)
   end
 
 end
--- p2-app/app/models/user.rb
+++ p2-app/app/models/user.rb
@@ -7,4 +7,9 @@ class User < ActiveRecord::Base
   validates_presence_of :username, :first_name, :password, :score
   validates_uniqueness_of :username, :email
 
+  after_initialize :defaults
+  def defaults
+  	self.score = 0
+  	self.admin = false
+  end
 end
