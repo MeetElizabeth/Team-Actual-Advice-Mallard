c3d8374c0f151e6dc201e8adb2bfe371b4c935d3
hoanewton <caohoa86hp@gmail.com>
2014-08-04 12:28:53 -0400

    add welcome page, add buttons sign up, log in, log out. Add sessions routes

--- p2-app/config/routes.rb
+++ p2-app/config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
   resources :animals, only: :show
   resources :games, only: [:index, :show]
 
+  get '/', to: 'sessions#new'
   get '/users/:id/animals', to: 'users#animals'
+
+  # Sessions
+  get '/login' => 'sessions#new'
+  post '/sessions' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
 end
