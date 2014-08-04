709c50a01d11242d46ed3d342f4617f07000928e
ppakdev <ppak73@gmail.com>
2014-08-04 15:56:56 -0400

    initial stages for guessable letters method

--- p2-app/app/models/game.rb
+++ p2-app/app/models/game.rb
@@ -2,3 +2,30 @@ class Game < ActiveRecord::Base
   belongs_to :user
   has_one :animal
 end
+
+
+
+
+
+
+
+
+
+
+
+
+## for populating guessable letters
+  def guessable_letters
+    # animal[0] = { name: 'hippo'}  ==> animal[0]["name"]
+    animals = ['hippo', 'duck', 'frog']
+    alphabet = *('a'..'z')
+
+    letter_array = animals[0].split('').shuffle
+
+    while letter_array.length < 12
+      letter_array.push(alphabet.sample)
+    end
+
+    letter_array.shuffle.map
+
+  end
