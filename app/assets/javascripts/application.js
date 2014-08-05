41d39c8070b1b481584a2154added56ccc15fda2
ppakdev <ppak73@gmail.com>
2014-08-05 19:51:54 -0400

    letters append and check solution sorta work

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,10 +18,44 @@
 
 // going to have to be moved to games/show.js maybe?
 $(function() {
-  console.log('howdy');
-  $('.letter').draggable({ cursor: "move", snap: $('.blank-space') });
-  $('.blank-space').droppable();
-  $('.letter').mousedown(function(){
-    $('.letter').css('cursor', 'grabbing');
-  });
+  console.log('what does the fox say?');
+  gamePlay();
+  $('body').on('click', '#check-button', checkSolution);
 })
+
+
+function gamePlay() {
+  var numLetters = $('.letter-bank').children().length;
+  var numSpaces = $('.guess-area').children().length;
+  var draggedLetter;
+  for (var i = 0; i < numLetters; i++) {
+    $('#letter-'+(i+1)+'').draggable({
+      cursor: 'move',
+      revert: true
+    })
+  }
+  for (var b = 0; b < numSpaces; b++) {
+    $('#space-'+(b+1)+'').droppable({
+      accept: '.letter',
+      tolerance: 'touch',
+      drop: function (event, ui) {
+        var draggedLetter = ui.draggable.text();
+        $(this).replaceWith(ui.draggable)
+      }
+    });
+  }
+}
+
+function checkSolution() {
+  var guessArea = $('.guess-area').children();
+  var answer = $('.answer').attr('id');
+  var guess = '';
+  for (var i = 0; i < guessArea.length; i++) {
+    guess += guessArea.eq(i).text();
+  };
+  if (guess === answer) {
+    alert('You did it!');
+  } else {
+    console.log('idiot');
+  }
+}
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,10 +7,6 @@ class GamesController < ApplicationController
     @game = Game.new
     @animal = Animal.find(params[:id])
     @playable_letters = @game.guessable_letters(@animal)
-    respond_to do |format|
-      format.html {}
-      format.js {render :layout => false}
-    end
   end
 
 end
