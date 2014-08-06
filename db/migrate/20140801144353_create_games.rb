a4eac14efaa7947d2cc7b5e2eff5960a20d4c2b7
ppakdev <ppak73@gmail.com>
2014-08-06 16:03:36 -0400

    removed column

--- db/migrate/20140801144353_create_games.rb
+++ db/migrate/20140801144353_create_games.rb
@@ -2,7 +2,6 @@ class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
       t.integer :points
-      t.integer :level
       t.text :images
       t.string :words
       t.string :letters
