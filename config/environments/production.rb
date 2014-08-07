f911b960c18b3745cd17020b5e7a1ec6b09c5822
ppakdev <ppak73@gmail.com>
2014-08-07 10:50:33 -0400

    action_mailer env stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,15 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                587,
+    domain:              'example.com',
+    user_name:            ENV["GMAIL_USERNAME"],
+    password:            ENV["GMAIL_PASSWORD"],
+    authentication:      'plain',
+    enable_starttls_auto: true  }
   config.action_mailer.default_url_options = { :host => 'http://secure-bayou-5911.herokuapp.com' }
 
 
